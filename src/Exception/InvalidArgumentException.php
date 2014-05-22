ec2d4c56d6e573c2b20f3a305692e6b7022e86f9
eriwin <erikvanwingerden@hotmail.com>
2014-05-22 15:11:59 +0200

    Update InvalidArgumentException.php

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -5,10 +5,9 @@
 * For the full copyright and license information, please view the LICENSE
 * file that was distributed with this source code.
 */
-
 namespace Wb\GoogleAnalyticsCookieParser\Exception;
 
-
-class InvalidArgumentException extends \InvalidArgumentException {
+class InvalidArgumentException extends \InvalidArgumentException 
+{
 
 }
